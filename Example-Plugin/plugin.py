d401dce4942870a5ada22f6919221ca9b6811da8
Aitor Emper <74368135+aitronssesin@users.noreply.github.com>
2024-02-06 16:38:26 +0100

    Add files via upload

---
+++ Example-Plugin/plugin.py
@@ -0,0 +1,7 @@
+import gradio as gr
+
+def applio_plugin():
+    gr.Markdown(
+        value=
+            "This code snippet introduces an Applio plugin. The heart of the plugin lies in the `def applio_plugin()` function, acting as the interface for the Gradio tab. This function will be brought into the plugins tab later on. It's crucial to maintain the original names of both the function and the `plugin.py` file, as they are integral to the import process. Additionally, there's a requirements file that cannot be relocated or renamed but can be removed if not needed."
+    )
